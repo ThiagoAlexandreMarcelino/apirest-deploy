2261ddbfd4ce5431d8ef8982e949ffbf03b36c62
Thiago A Marcelino <thiagodistend@hotmail.com>
2020-09-27 19:59:49 -0300

    Adicionado CORS

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const mongose = require("mongoose")
 const requireDir = require('require-dir')
-
+const cors = require('cors');
 
 
 //iniciando o APP
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //iniciando o DB
 mongose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser:true,useUnifiedTopology:true});
